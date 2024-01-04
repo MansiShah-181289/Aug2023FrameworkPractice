03d4c02cff2fe964b2cebf9de9aa08639c9bdecc
MansiShah-181289 <mansishahit18@gmail.com>
2024-01-04 16:47:18 +0530

    Added By locator to the carpage

--- src/main/java/com/qa/opencart/pages/Cartpage.java
+++ src/main/java/com/qa/opencart/pages/Cartpage.java
@@ -1,6 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class Cartpage {
 
-	int i =10;
+	private By email = By.id("Index.id");
+	
+	public void addCart() {
+		System.out.println("My Id is:" + email);
+	}
 }
